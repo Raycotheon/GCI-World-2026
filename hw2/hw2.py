7b0ecdf18b8f2d0475a759db170ce8e4cacb47ff
Raycotheon <johnricorbacayb@gmail.com>
2026-04-26 12:04:22 +0800

    hw2

---
+++ hw2/hw2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+# Libraries for retrieving data from the web and handling zip files
+import requests, zipfile
+from io import StringIO
+import io
+
